31a01d021256216fe603fdd4283e2ee83c0e65ad
smpower <bzsjxhywrf@outlook.com>
2017-08-01 11:48:06 +0800

    修改 gulpfile.js 文件

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,11 @@ var gulp = require("gulp"),
     less = require("gulp-less");
 
 gulp.task("less", function () {
-    gulp.src("less/*.less")
+    gulp.src("src/less/*.less")
         .pipe(less())
         .pipe(gulp.dest("css"));
 });
 
 gulp.task("default", function () {
-    return gulp.watch("less/*.less", ["less"]);
+    return gulp.watch("src/less/*.less", ["less"]);
 });
