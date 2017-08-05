9585105d304c11d33d877f755bd420c71d7aa17c
ora-emotion <contact@ruofei.me>
2017-08-05 17:23:13 +0800

    解决【定制爱情】右侧菜单不显示的 bug

--- js/custom-love.js
+++ js/custom-love.js
@@ -1,104 +1,81 @@
-window.onload = function () {
+// 获取模块一部分元素
+//--> 获取 li 元素
+var moduleOneLi = $(".module-01 .content li");
+//--> 列表标题 moduleOneListTitle
+var moduleOneListTitle = $(".module-01 .content .txt .title");
+//--> 图片右边的箭头 moduleOneArrow
+var moduleOneArrow = $(".module-01 .content li svg.arrow");
+//--> 列表前面的数字符号 moduleOnelistNum
+var moduleOnelistNum = $(".module-01.content .li svg.triangle");
+//--> 获取图片列表的父级元素
+var picsGroup = $(".module-01 .content .pics-group");
+//--> 获取 img 元素
+var pic = $(".module-01 .content .pics-group img");
 
-    // 获取模块一部分元素
-    //--> 获取 li 元素
-    var moduleOneLi = $(".module-01 .content li");
-    //--> 列表标题 moduleOneListTitle
-    var moduleOneListTitle = $(".module-01 .content .txt .title");
-    //--> 图片右边的箭头 moduleOneArrow
-    var moduleOneArrow = $(".module-01 .content li svg.arrow");
-    //--> 列表前面的数字符号 moduleOnelistNum
-    var moduleOnelistNum = $(".module-01.content .li svg.triangle");
-    //--> 获取图片列表的父级元素
-    var picsGroup = $(".module-01 .content .pics-group");
-    //--> 获取 img 元素
-    var pic = $(".module-01 .content .pics-group img");
+/* 模块一动效 */
+function moduleOne() {
+    moduleOneListTitle.mouseover(function () {
+        //--> 获取当前 li 元素的下标 listIndex
+        var listIndex = $(this).parent().parent().index();
 
-    /* 模块一动效 */
-    function moduleOne() {
-        moduleOneListTitle.mouseover(function () {
-            //--> 获取当前 li 元素的下标 listIndex
-            var listIndex = $(this).parent().parent().index();
+        //--> 当前元素的兄弟元素恢复为原来的颜色
+        // $(moduleOneLi[listIndex]).siblings().find(".txt .title").animate({
+        //     "color": "#2d2d2d"
+        // }, 300);
 
-            //--> 当前元素的兄弟元素恢复为原来的颜色
-            // $(moduleOneLi[listIndex]).siblings().find(".txt .title").animate({
-            //     "color": "#2d2d2d"
-            // }, 300);
+        //--> 当前元素的文字变为红色
+        // $(this).animate({
+        //     "color": "#fd3232"
+        // }, 300);
 
-            //--> 当前元素的文字变为红色
-            // $(this).animate({
-            //     "color": "#fd3232"
-            // }, 300);
+        //--> 当前 li 的兄弟元素下的小箭头隐藏 - 不透明度 0
+        $(moduleOneLi[listIndex]).siblings().find("svg.arrow").animate({
+            "opacity": "0"
+        }, 300);
 
-            //--> 当前 li 的兄弟元素下的小箭头隐藏 - 不透明度 0
-            $(moduleOneLi[listIndex]).siblings().find("svg.arrow").animate({
-                "opacity": "0"
-            }, 300);
-
-            //--> 当前 li 下的小箭头显示 - 不透明度 1
-            $(moduleOneLi[listIndex]).find("svg.arrow").animate({
-                "opacity": "1"
-            }, 300);
+        //--> 当前 li 下的小箭头显示 - 不透明度 1
+        $(moduleOneLi[listIndex]).find("svg.arrow").animate({
+            "opacity": "1"
+        }, 300);
 
 
-            console.log($(pic[listIndex]).siblings());
+        console.log($(pic[listIndex]).siblings());
 
-            $(pic[listIndex]).siblings().css({
-                "display": "none"
-            }).animate({
-                "opacity": "0"
-            }, 500);
-            $(pic[listIndex]).css({
-                "display": "block",
-                "opacity": "0"
-            }).animate({
-                "opacity": "1"
-            }, 500);
+        $(pic[listIndex]).siblings().css({
+            "display": "none"
+        }).animate({
+            "opacity": "0"
+        }, 500);
+        $(pic[listIndex]).css({
+            "display": "block",
+            "opacity": "0"
+        }).animate({
+            "opacity": "1"
+        }, 500);
 
+    });
+
+}
+moduleOne();
+
+// 获取模块二部分元素
+//--> 获取缩略图
+var smImg = $(".module-06 .main .img-sm img");
+//--> 获取大图(背景)
+var lgImgBg = $(".module-06 .main .img-lg");
+console.log(lgImgBg);
+
+/* 模块六动效 */
+function moduleSix() {
+    smImg.mouseover(function () {
+        //--> 获取当前缩略图的索引值
+        var smImgIndex = $(this).index();
+        // console.log(smImgIndex);
+        lgImgBg.css({
+            "backgroundImage": "url('img/custom-love/m06-pic0" + (smImgIndex+1) + "-lg.png')"
         });
+        // console.log(lgImgBg.css("backgroundImage"));
+    });
+}
+moduleSix();
 
-    }
-    moduleOne();
-
-    // 获取模块二部分元素
-    //--> 获取缩略图
-    var smImg = $(".module-06 .main .img-sm img");
-    //--> 获取大图(背景)
-    var lgImgBg = $(".module-06 .main .img-lg");
-    console.log(lgImgBg);
-
-    /* 模块六动效 */
-    function moduleSix() {
-        smImg.mouseover(function () {
-            //--> 获取当前缩略图的索引值
-            var smImgIndex = $(this).index();
-            // console.log(smImgIndex);
-            lgImgBg.css({
-                "backgroundImage": "url('img/custom-love/m06-pic0" + (smImgIndex+1) + "-lg.png')"
-            });
-            // console.log(lgImgBg.css("backgroundImage"));
-        });
-    }
-    moduleSix();
-
-    // 获取模块八部分元素
-    //--> 获取每个 step 元素
-    var step = $(".module-08 .main .line .step");
-    // console.log(step);
-
-    /* 模块八动效 */
-    // function moduleEight() {
-    //     step.mouseover(function () {
-    //         //--> 获取当前元素的索引值
-    //         var stepIndex = $(this).index();
-    //
-    //         $(step[stepIndex]).children().css({
-    //             "color": "#fd3232"
-    //         }).parent().children(".num").css({
-    //             "color": "#fd3232"
-    //         });
-    //     });
-    //
-    // }
-    // moduleEight();
-};
