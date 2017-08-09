c234362dbf0dced0e0487d183ff8f89902a4b134
ora-emotion <contact@ruofei.me>
2017-08-09 16:13:04 +0800

    [WIP] 完成【关于我们】第三个模块“橘子环境”

---
+++ js/about-us.js
@@ -0,0 +1,21 @@
+/*
+* created by smpower
+* email: bzsjxhywrf@outlook.com
+* github: https://github.com/smpower/
+* */
+
+/* 模块三：橘子环境 */
+function moduleThree() {
+    /* 轮播图 */
+    var moduleThreeBanner = new Swiper(".swiper-container", {
+        autoplay: 3000,
+        prevButton:'.swiper-button-prev',
+        nextButton:'.swiper-button-next',
+        loop: true,
+    });
+
+
+
+}
+moduleThree();
+/* /模块三：橘子环境 */
