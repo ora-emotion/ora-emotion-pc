cddf7c0beff54fcf2c4276010f8c79df0b9a3930
jzwgit <706861648@qq.com>
2017-08-09 14:03:14 +0800

    开始关于我们框架搭建工作

--- js/index.js
+++ js/index.js
@@ -1,80 +1,71 @@
 $(document).ready(function() {
 
+	// 元素到可视区域时再显示
+	(function() {
+		function lazyLoad() {
+			/**
+			 * Created by smpower on 2017/8/3.
+			 * email: bzsjxhywrf@outlook.com
+			 * GitHub: https://github.com/smpower/
+			 */
 
-    // 元素到可视区域时再显示
-	(function () {
-        function lazyLoad() {
-            /**
-             * Created by smpower on 2017/8/3.
-             * email: bzsjxhywrf@outlook.com
-             * GitHub: https://github.com/smpower/
-             */
+			// 显示微信二维码
+			function qrcodeWechat() {
+				// 淡入
+				function wechatFadeIn() {
+					$(".wechat").fadeIn(300);
+				}
+				// 淡出
+				function wechatFadeOut() {
+					$(".wechat").fadeOut(300);
+				}
+				// 划过图标显示
+				$(".qrcode-wechat img").mouseover(function() {
+					wechatFadeIn();
+				});
+				$(".qrcode-wechat img").mouseout(function() {
+					wechatFadeOut();
+				});
+				// 划过文字显示
+				$(".pull-right .txt p:first-child").mouseover(function() {
+					wechatFadeIn();
+				});
+				$(".pull-right .txt p:first-child").mouseout(function() {
+					wechatFadeOut();
+				});
+			}
+			qrcodeWechat();
 
-            // 显示微信二维码
-            function qrcodeWechat() {
-                // 淡入
-                function wechatFadeIn() {
-                    $(".wechat").fadeIn(300);
-                }
-                // 淡出
-                function wechatFadeOut() {
-                    $(".wechat").fadeOut(300);
-                }
-                // 划过图标显示
-                $(".qrcode-wechat img").mouseover(function () {
-                    wechatFadeIn();
-                });
-                $(".qrcode-wechat img").mouseout(function () {
-                    wechatFadeOut();
-                });
-                // 划过文字显示
-                $(".pull-right .txt p:first-child").mouseover(function () {
-                    wechatFadeIn();
-                });
-                $(".pull-right .txt p:first-child").mouseout(function () {
-                    wechatFadeOut();
-                });
-            }
-            qrcodeWechat();
+			// 获取当前文档高度
+			var documentHeight = $(document).height();
+			// 获取当前屏幕高度
+			var windowHeight = $(window).height();
+			// 获取第七个模块距离屏幕顶端的高度
+			var moduleSevenOffsetTop = $(".module-07 .main").offset().top;
+			//          console.log(moduleSevenOffsetTop);
 
+			$(window).scroll(function() {
+				// 获取滚动条滚动的距离
+				var scrollTop = $(window).scrollTop();
+				console.log(scrollTop);
 
-            // 获取当前文档高度
-            var documentHeight = $(document).height();
-            // 获取当前屏幕高度
-            var windowHeight = $(window).height();
-            // 获取第七个模块距离屏幕顶端的高度
-            var moduleSevenOffsetTop = $(".module-07 .main").offset().top;
-            console.log(moduleSevenOffsetTop);
+				if(scrollTop > (moduleSevenOffsetTop - windowHeight / 3)) {
+					// alert("ok");
+					$(".module-07 .main .top").animate({
+						left: "13.5%", 
+						opacity: "1"
+					}, 1000);
+					$(".module-07 .main .bot").animate({
+						left: "0",
+						opacity: "1"
+					}, 1000);
+				}
+			});
+		}
+		lazyLoad();
+	})();
 
-            $(window).scroll(function () {
-                // 获取滚动条滚动的距离
-                var scrollTop = $(window).scrollTop();
-                console.log(scrollTop);
-
-                if (scrollTop > (moduleSevenOffsetTop - windowHeight/3)) {
-                    // alert("ok");
-                    $(".module-07 .main .top").animate({
-                        left: "13.5%",
-                        opacity: "1"
-                    }, 1000);
-                    $(".module-07 .main .bot").animate({
-                        left: "0",
-                        opacity: "1"
-                    }, 1000);
-                }
-            });
-        }
-        lazyLoad();
-    })();
-
-
-
-
-
-
-
-
-    $(".li21").mouseover(function() {
+	$(".li21").mouseover(function() {
 		$(".li11").css({
 			"border": "2px slide #FFFFFF",
 			"box-shadow": "0 5px 10px #FFFFFF",
@@ -164,52 +155,93 @@ $(document).ready(function() {
 			"box-shadow": "none"
 		});
 	});
+});
 
-	/* 导师部分 - 轮播图 */
-    // (function () {
-    //     var n = 0;                                          // 记录索引
-    //     var mentorItem = $(".mentor-item .inner .item");
-    //     var prevBtn = $("#prevTeam");
-    //     var nextBtn = $("#nextTeam");
-    //     var timer = setInterval(run, 3000);
-    //
-    //     // run();
-    //
-    //     function run(num) {
-    //         n++;
-    //         if (n == mentorItem.length) {
-    //             n = 0;
-    //         }
-    //
-    //         $(mentorItem[n]).siblings().removeClass("active");
-    //         $(mentorItem[n]).addClass("active");
-    //         $(mentorItem[n]).find("img").animate({
-    //             top: "-45px"
-    //         }, "fast");
-    //     }
-    //     // 点击按钮向前翻页
-    //     prevBtn.click(function () {
-    //         n--;
-    //         if (n < 0) {
-    //             n = mentorItem.length - 1;
-    //             $(mentorItem[n]).addClass("active").siblings().removeClass("active");
-    //         } else {
-    //             $(mentorItem[n]).addClass("active").siblings().removeClass("active");
-    //         }
-    //         // console.log(n);
-    //     });
-    //
-    //     // 点击按钮，向后翻页
-    //     nextBtn.click(function () {
-    //         n++;
-    //         console.log(n);
-    //         if (n === mentorItem.length) {
-    //             n = 0;
-    //             $(mentorItem[n]).addClass("active").siblings().removeClass("active");
-    //         }
-    //         $(mentorItem[n]).addClass("active").siblings().removeClass("active");
-    //         // console.log(n);
-    //     })
-    //
-    // })();
+
+$(document).ready(function() {
+	// 获取当前文档高度
+	var documentHeight = $(document).height();
+	// 获取当前屏幕高度
+	var windowHeight = $(window).height();
+	// 获取为什么失去爱情模块距离屏幕顶端的高度
+	var moduleSevenOffsetTop1 = $(".wailian-ul").offset().top;
+	//获取橘子总结的经验模块距离屏幕顶端的高度
+	var moduleSevenOffsetTop2 = $(".body3-ul").offset().top;
+	//获取橘子总结底部的经验模块距离屏幕顶端的高度
+	var moduleSevenOffsetTop3 = $(".body4-wen").offset().top;
+	//获取给您的保障模块距离屏幕顶端的高度
+	var moduleSevenOffsetTop4 = $(".txt-list").offset().top;
+//	console.log(moduleSevenOffsetTop);
+	$(window).scroll(function() {
+		// 获取滚动条滚动的距离
+		var scrollTop1 = $(window).scrollTop();
+	
+		//高级方案模块
+		if(scrollTop1 > (moduleSevenOffsetTop1 - windowHeight + 500)) {
+			// alert("ok");
+			$(".ma-left1").animate({
+				left: "100px",
+				opacity: "1"
+			}, 1000);
+		}
+		
+	});
+	$(window).scroll(function() {
+		// 获取滚动条滚动的距离
+		var scrollTop2 = $(window).scrollTop();
+	
+		//高级方案模块
+		if(scrollTop2 > (moduleSevenOffsetTop2 - windowHeight + 500)) {
+			// alert("ok");
+			$(".body3-ul1").animate({
+				left: "0%",
+				opacity: "1"
+			}, 1000);
+			$(".body3-ul2").animate({
+				right: "0%",
+				opacity: "1"
+			}, 1000);
+		}
+	});
+	$(window).scroll(function() {
+		// 获取滚动条滚动的距离
+		var scrollTop3 = $(window).scrollTop();
+	
+		//高级方案模块
+		if(scrollTop3 > (moduleSevenOffsetTop3 - windowHeight + 500)) {
+			// alert("ok");
+			$(".body4-wen-li1").animate({
+				bottom: "-100px",
+				opacity: "1"
+			}, 1500);
+			$(".body4-wen-li2").animate({
+				bottom: "-100px",
+				opacity: "1"
+			}, 1000);
+			$(".body4-wen-li3").animate({
+				bottom: "-100px",
+				opacity: "1"
+			}, 1500);
+		}
+	});
+	$(window).scroll(function() {
+		// 获取滚动条滚动的距离
+		var scrollTop4 = $(window).scrollTop();
+	
+		//高级方案模块
+		if(scrollTop4 > (moduleSevenOffsetTop4 - windowHeight + 500)) {
+			$(".txt-list-div1").animate({
+				right: "12%",
+				opacity: "1"
+			}, 1000);
+			$(".txt-list-div2").animate({
+				right: "14%",
+				opacity: "1"
+			}, 1500);
+			$(".txt-list-div3").animate({
+				right: "15%",
+				opacity: "1"
+			}, 2000);
+		}
+	});
 });
