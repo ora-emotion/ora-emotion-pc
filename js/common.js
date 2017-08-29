5eb87ec2cd8cf80da66d0aa7ff8bd20ecd969029
smpower <bzsjxhywrf@outlook.com>
2017-08-29 15:39:14 +0800

    更更改【课程介绍】为【服务介绍】

--- js/common.js
+++ js/common.js
@@ -45,3 +45,23 @@ $(function() {
         $(".yb_bar .yb_ercode").css('height', '53px');
     });
 });
+
+
+function navJson() {
+    // 获取二级菜单列表
+    var navList = $(".nav .nav-block li");
+
+    $.ajax({
+        type: "GET",
+        url: "../data/nav.json",
+        success: function (data) {
+            var i = 0;
+            // 依次将获取到的对象的属性值插入到 li 元素中的 a 标签里
+            for (var titleName in data.navTitle) {
+                $(navList[i]).children("a").text(data.navTitle[titleName]);  // 通过方括号语法来访问对象的属性值
+                i++;
+            }
+        }
+    });
+}
+navJson();
