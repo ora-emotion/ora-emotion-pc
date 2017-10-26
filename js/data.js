f6b7df7c9185b25aa76442d159e0ce7c0bf307e0
smpower <bzsjxhywrf@outlook.com>
2017-10-26 10:41:36 +0800

    测试并实现更新页面 title 功能

--- js/data.js
+++ js/data.js
@@ -13,13 +13,14 @@
 
 var data = (function () {
   var
-    configMap = {
-      title : {}
+    configMap = {},
+    stateMap = {
+      $html : null,
+      url   : []
     },
-    stateMap = { $html : null },
     jqueryMap = {},
 
-    setJqueryMap, updateTitle, updateKeywords,
+    setJqueryMap, getAnchor,   updateTitle, updateKeywords,
     updateDes,    updateData,  getTitle,       initModule;
 
   setJqueryMap = function () {
@@ -33,12 +34,28 @@ var data = (function () {
     };
   };
 
-  // 获取 title 数据
-  getTitle = function () {
+  // Start : getAnchor()
+  // 功能  : 获取并处理地址栏 url，处理后的地址栏类似于 ['pc', 'index'] 的形式
+  //
+  getAnchor = function () {
+    var
+      url    = window.location.href;
+      url    = url.split('/');                              // 分隔 url 为数组
+      url    = [url[url.length - 2], url[url.length - 1]];  // 取得数组倒数第二项元素和最后一项: xxx.html
+      url[1] = url[1].split('.')[0];
+
+    stateMap.url = url;
+  };
+  // End : getAnchor();
+
+  // Start : getTitle()
+  // 功能  : 获取 title.json 数据，将获取到的 data 作为参数传递到 updateTitle()
+  //
+  getTitle = function (href) {
     $.ajax({
       type     : 'GET',
       dataType : 'json',
-      url      : 'data/title.json',
+      url      : href,
       success  : function (data) {
         updateTitle(data);
       },
@@ -47,22 +64,58 @@ var data = (function () {
       }
     });
   };
+  // End : getTitle()
 
-  // 更新 title
+  // Start : updateTitle()
+  // 功能  : 更新页面 title 的值
+  //
   updateTitle = function (data) {
-    console.log(data);
-  };
+    var
+      key,
+      title,
+      url = stateMap.url;
 
-  updateData = function () {
-    getTitle();
-  };
+    // url: ['xxx', 'xxx.html']
+    switch (url[0]) {
+      case 'pc' || 'www.jvziqinggan.com' :
+        jqueryMap.$title.text(data[url[1]]);
+        break;
+      case 'views' :
+        jqueryMap.$title.text(data.views[url[1]]);
+        break;
+      case 'news' :
+        jqueryMap.$title.text(data.news[url[1]]);
+        break;
+      case 'anli' :
+        jqueryMap.$title.text(data.anli[url[1]]);
+        break;
+      default:
+        break;
+    }
 
-  initModule = function ($html) {
+  };
+  // End : updateTitle()
+
+  // Start : updateData()
+  // 功能  : 更新页面数据
+  //
+  updateData = function (href) {
+    getTitle(href);
+  };
+  // End : updateData()
+
+  // Start : initModule()
+  // 功能  : 初始化模块
+  //
+  initModule = function ($html, href) {
     stateMap.$html = $html;
 
     setJqueryMap();
-    updateData();
+    getAnchor();
+    updateData(href);
   };
+  // End : initModule();
 
+  // 导出公开方法
   return { initModule : initModule };
 }());
